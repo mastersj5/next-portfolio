ac42de1e11d1232327bc4340c5302d8e8d0c704d
Joshua Masters <masters.josh2019@gmail.com>
2026-02-04 23:46:48 -0500

    Adjust group position in Home component for better alignment

--- app/page.tsx
+++ app/page.tsx
@@ -120,7 +120,7 @@ export default function Home() {
               </Center>
             </RigidBody>
             
-            <group position={[1.5, -.5, -2]}>  
+            <group position={[0, -.5, -2]}>  
 
               <Center top>
                   <Laptop_alienpredator 
